aed01f6835d1209991faef1c30b7889ab0b33cea
choudel <chouaibh@gmail.com>
2022-05-03 05:48:01 +0100

    having fun

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,11 @@
+<header class="container mx-auto px-4 bg-pink-300 ">
+    <div class="block pt-10 sm:pt-0">
+        <ul class="block p-3 items-center sm:flex sm:flex-wrap "id="list-nav">
+            <li class="my-2 mr-6"><a class="pill" href="/home">Home</a></li>      
+            <li class="my-2 mr-6"><a class="pill" href="/about">About Us</a></li>      
+            <li class="my-2 mr-6"><a class="pill" href="/services">Services</a></li>      
+            <li class="my-2 mr-6"><a class="pill" href="/products">Products</a></li>      
+            <li class="my-2 mr-6"><a class="pill" href="/contact">Contact</a></li>      
+          </ul>
+    </div>
+</header>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,7 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/components/header.svelte';
 </script>
 
-<header>
-	<nav class="container flex gap-10">
-		<div class="flex basis-1/3">
-			<img src="sensei logo.svg" alt="sensei" /><span>EL SENSEI</span>
-		</div>
-
-		<ul class=" flex basis-1/3 gap-10">
-			<li class="cursor-pointer">Features</li>
-			<li class="cursor-pointer">Pricing</li>
-			<li class="cursor-pointer">Contact</li>
-			<button type="button" class="bg-ted text-white px-7 py-3 uppercase rounded-md left-2">login</button>
-		</ul>
-	</nav>
-</header>
+<Header></Header>
 <slot />
